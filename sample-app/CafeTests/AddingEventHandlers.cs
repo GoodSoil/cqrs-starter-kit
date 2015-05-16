03ab600548e8972178f79c7c871c4c58a4c7ad52
dagilleland <dagilleland@shaw.ca>
2015-05-16 09:48:31 -0600

    Working on Issue #2
    The test is failing - need to properly test async event handling

---
+++ sample-app/CafeTests/AddingEventHandlers.cs
@@ -0,0 +1,53 @@
+﻿using Cafe.Tab;
+using Edument.CQRS;
+using Events.Cafe;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebFrontend;
+
+namespace CafeTests
+{
+    [TestFixture]
+    public class AddingEventHandlers
+    {
+        [Test]
+        public void ShouldAddEventHandlers()
+        {
+            // Arrange
+            Guid testId = Guid.NewGuid();
+            var command = new OpenTab()
+            {
+                Id = testId,
+                TableNumber = 5,
+                Waiter = "Bob"
+            };
+            var expectedEvent = new TabOpened()
+            {
+                Id = testId,
+                TableNumber = 5,
+                Waiter = "Bob"
+            };
+            ISubscribeTo<TabOpened> handler = new EventHandler();
+
+            // Act
+            Domain.Dispatcher.AddSubscriberFor<TabOpened>(handler);
+            Domain.Dispatcher.SendCommand(command);
+
+            // Assert
+            Assert.AreEqual(expectedEvent.Id, (handler as EventHandler).Actual.Id);
+        }
+
+        public class EventHandler : ISubscribeTo<TabOpened>
+        {
+            public TabOpened Actual { get; private set; }
+            public void Handle(TabOpened e)
+            {
+                Actual = e;
+            }
+        }
+    }
+}
